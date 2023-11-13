7df59968f158e6def7357a4778cf232675b696c2
문정환 <hardluck0417@gmail.com>
2023-11-13 16:45:56 +0900

    preprocess01_rgba_to_grayScale - v1.0 preprocess02_resize_img.py - v1.0
    4채널 이미지를 0~255의 흑백이미지로 바꾼 뒤
    0~255의 이미지를 0과 1의 흑백 이미지로 바꿔주는
    rgba_to grayScale.py와
    
    rgba_to grayScale.py의 결과물들을 1024,1024,1로 리사이징하는 resize_img.py와 그 결과물들

---
+++ Moon_test_folder/preprocess01_rgba_to_grayScale.py
@@ -0,0 +1,21 @@
+from PIL import Image, ImageOps
+import os
+
+sheet_folder = "./sheets"
+
+file_list = os.listdir(sheet_folder)
+for file in file_list:
+    file_path = os.path.join(sheet_folder, file)
+    image = Image.open(file_path)
+    rgba_image = image.convert("RGBA")
+    new_image = Image.new("RGB", rgba_image.size, "WHITE")  # 흰색 배경으로 새 이미지 생성
+    for x in range(rgba_image.size[0]):
+        for y in range(rgba_image.size[1]):
+            r, g, b, a = rgba_image.getpixel((x, y))
+            if a != 0:  # 알파 값이 0이 아니면, 즉 투명하지 않으면
+                new_image.putpixel((x, y), (0, 0, 0))  # 해당 위치를 검은색으로 설정
+
+    # 이미지를 이진 모드로 변환
+    binary_image = new_image.convert('1')
+    # 변환된 이미지 저장
+    binary_image.save(f'./binary_sheets/binary_{file}')
